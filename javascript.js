c1857483bf53617f302d23107627b4f1b8fc3dc5
HeidiHubbard <77553856+HeidiHubbard@users.noreply.github.com>
2021-04-04 16:17:56 -0400

    First commit

---
+++ javascript.js
@@ -0,0 +1,89 @@
+const typingAnimationDuration = 2000;
+const scrollDelay = 2000;
+const scrollDuration = 750;
+
+$(document).ready(() => {
+  $(".hamburger-btn, .nav a").on('click', () => {
+    $(".hamburger-btn, .nav").toggleClass('active').toggleClass('inactive');
+  })
+  
+	const typeText = (target, text, step, duration) => {
+  	if(step > text.length) {
+    	return;
+    }
+    
+  	$(target).text(text.substr(text, step));
+    
+    setTimeout(() => {
+    	typeText(target, text, step + 1, duration);
+    }, duration / text.length);
+  };
+
+	$("[typingText]").each(function() {
+  	const text = $(this).attr('typingText');
+    
+    if(!text) {
+      return;
+    }
+    
+    const steps = text.length;
+    typeText(this, text, 1, typingAnimationDuration);
+  });
+
+  $('html, body').delay(typingAnimationDuration + scrollDelay).animate({
+    scrollTop: $("#about").offset().top - 40
+  }, scrollDuration);
+
+  const fadeInOnScroll = (target) => {
+    $(window).on('scroll', () => {
+      checkFadeInOnScroll(target);
+    });
+
+    checkFadeInOnScroll(target);
+  };
+
+  const checkFadeInOnScroll = (target) => {
+  	if($(target).hasClass('fade-in')) {
+      return;
+    }
+    
+    const topOffset = $(target).offset().top;
+    const windowHeight = $(window).height();
+    const topOfWindow = $(window).scrollTop();
+    console.log(topOfWindow + windowHeight, topOffset)
+    if (topOfWindow + windowHeight - 100 > topOffset) {
+      $(target).addClass("fade-in");
+    }
+  }
+
+  $(".fade-in-on-scroll").each(function() {
+    fadeInOnScroll(this);
+  });
+
+  const slideInOnScroll = (target) => {
+    $(window).on('scroll', () => {
+      checkSlideInOnScroll(target);
+    });
+
+    checkSlideInOnScroll(target);
+  };
+
+  const checkSlideInOnScroll = (target) => {
+  	if($(target).hasClass('slide-in')) {
+      return;
+    }
+    
+    const topOffset = $(target).offset().top;
+    const windowHeight = $(window).height();
+    const topOfWindow = $(window).scrollTop();
+    console.log(topOfWindow + windowHeight, topOffset)
+    if (topOfWindow + windowHeight - 100 > topOffset) {
+      $(target).addClass("slide-in");
+    }
+  }
+
+  $(".slide-in-on-scroll").each(function() {
+    slideInOnScroll(this);
+  });
+});
+
